5abe6c20acbf5e32987b436a4e187bd8647f5149
Juanvalencia112 <j.valenciam>
2024-04-05 20:38:16 -0500

    Main y formato respuesta

--- trabajoEnClase.py
+++ trabajoEnClase.py
@@ -1,10 +1,8 @@
-# Función para encontrar el conjunto de un elemento i
 def encontrar(padre, i):
     if padre[i] == i:
         return i
     return encontrar(padre, padre[i])
 
-# Función que une dos conjuntos en uno solo
 def union(padre, rango, x, y):
     x_raiz = encontrar(padre, x)
     y_raiz = encontrar(padre, y)
@@ -17,7 +15,6 @@ def union(padre, rango, x, y):
         padre[y_raiz] = x_raiz
         rango[x_raiz] += 1
 
-# Función principal del algoritmo de Kruskal
 def kruskal(vertices, aristas):
     resultado = []
 
@@ -29,7 +26,7 @@ def kruskal(vertices, aristas):
     e = 0
     i = 0
 
-    while e < vertices - 1 and i < len(aristas):  # Corregimos aquí
+    while e < vertices - 1 and i < len(aristas):
         u, v, w = aristas[i]
         i = i + 1
         x = encontrar(padre, u)
@@ -43,9 +40,7 @@ def kruskal(vertices, aristas):
     suma = 0
     for arista in resultado:
         suma += arista[2]
-    return suma
-
-
+    return suma, len(resultado)
 
 def bfs(grafo, inicio, nodos_a_visitar):
     visitados = set()
@@ -67,8 +62,6 @@ def bfs(grafo, inicio, nodos_a_visitar):
     nodos_no_visitados = [nodo for nodo in nodos_a_visitar if nodo not in visitados]
     return nodos_no_visitados
 
-
-
 def aereopuertos(destinos, cost, lista_aristas):
     lista=[]
     for i in range(1,destinos+1):
@@ -81,13 +74,18 @@ def aereopuertos(destinos, cost, lista_aristas):
         costo+=cost
         num+=1
     total=destinos-len(desconectados)
-    costo+=kruskal(len(lista), lista_aristas)+cost
-
+    costo_tree, num_airports = kruskal(len(lista), lista_aristas)
+    costo += costo_tree + cost
     return costo, num
 
-ejemplo=[(12, 9, 44),(7, 10, 19),(1, 12, 24),(7, 3, 53),(4, 10, 43),(9, 12, 56),(3, 1, 45),(4, 7, 62)]
+def main():
+    with open('input.txt', 'r') as f:
+        t = int(f.readline().strip())
+        for case in range(1, t + 1):
+            n, m, a = map(int, f.readline().strip().split())
+            aristas = [tuple(map(int, f.readline().strip().split())) for _ in range(m)]
+            costo, num_airports = aereopuertos(n, a, aristas)
+            print("Case #{}: {} {}".format(case, costo, num_airports))
 
-print(aereopuertos(12, 54, ejemplo))
-
-
-
+if __name__ == "__main__":
+    main()
